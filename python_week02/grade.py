d57e2fbefdb1d475e9cfaa94808d9c4141953d29
toplnwzatnk <67319010057@tnk.ac.th>
2024-10-22 13:39:15 +0700

    เกรด

---
+++ python_week02/grade.py
@@ -0,0 +1,19 @@
+a = int(input("กรุณากรอกคะแนน"))
+if a <0 or a > 100 :
+    print("ใส่เลข 0-100")
+elif a >=80:
+    print("เกรด4")
+elif a >=75:
+    print("เกรด3.5")
+elif a >=70:
+    print("เกรด3")
+elif a >65:
+    print("เกรด2.5")
+elif a >=60:
+    print("เกรด2")    
+elif a >=55:
+    print("เกรด1.5")
+elif a >=50:
+    print("เกรด1")   
+elif a >=50:
+    print("เกรด0")    
